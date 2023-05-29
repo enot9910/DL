531637a6ef9949431226fee8f7f9a688f9435a29
enot9910 <103646920+enot9910@users.noreply.github.com>
2023-05-29 09:20:23 +0300

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -2,25 +2,18 @@
 FROM ubuntu:20.04
 ARG DEBIAN_FRONTEND=noninteractive
 ENV DEBIAN_FRONTEND noninteractive
-#RUN apt update -y && apt upgrade -y
-#RUN apt install software-properties-common -y
-#RUN add-apt-repository ppa:deadsnakes/ppa -y
-#RUN apt install python3.8 python3-pip -y
 RUN apt-get update
 RUN apt-get install -y python3.7
 RUN apt-get install -y python3-pip
-#RUN apt install numpy -y
 RUN pip install opencv-python-headless
 RUN pip3 install tensorflow
 RUN pip3 install scikit-image
 RUN pip3 install keras
-RUN pip3 install pillow
-RUN pip3 install imagehash
 COPY test.py .
 COPY model.py .
 COPY data.py .
 COPY test_im ./test_im/
 COPY data ./data/
-COPY new_model.hdf5 .
+COPY model.hdf5 .
 CMD ["python3", "test.py"]
 
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from model import *
 from data import *
-from PIL import Image
-from imagehash import dhash
+import cv2
+import numpy as np
 
 
 '''
+from PIL import Image
+from imagehash import dhash
+
 testGene = testGenerator("data/membrane/test")
 new_model1 = load_model('unet_membrane.hdf5')
 results = new_model1.predict_generator(testGene, 5, verbose=1)
@@ -19,29 +22,23 @@ for i in range(2):
 if flag:
     print('Tests passed')
 else:
-    print('Something went wrong')
-'''    
-import os
-os.environ["NPY_ALLOW_DEPRECATED_API"] = "1"
-import cv2
-import numpy as np
+    print('Test failed')
+''' 
 
 testGene = testGenerator("data/membrane/test")
-new_model1 = load_model('new_model.hdf5')
+new_model1 = load_model('model.hdf5')
 results = new_model1.predict_generator(testGene, 5, verbose=1)
 saveResult("data/membrane/test", results)
 
 img1 = cv2.imread('data/membrane/test/' + str(1) + '_predict.png')
 img2 = cv2.imread('test_im/' + str(1) + '_test.png')
-
-# Computing the pixel-wise difference between the two images
 difference = cv2.subtract(img1, img2)
 
-# Computing image similarity (in percentages)
-similarity = (np.sum(difference == 0) / difference.size) * 100
+#MAE = np.sum(np.abs(np.subtract(img1,img2,dtype=np.float))) / img1.shape[0]
+MAE = np.sum(difference != 0) / img1.shape[0]
 
-if similarity > 95:
+if MAE < 0.05:
 	print("Test passed")
 else:
 	print("Test failed")
-print("Image similarity: ", similarity, "%")
+print("MAE: ", MAE)
